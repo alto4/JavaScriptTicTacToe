9b471f89f0813d2b5615da490088b87f01987eca
Scott Alton <scottaltonmusic@gmail.com>
2021-04-30 12:44:56 -0400

    Created reset logic upon winner. Styles updated.

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let counter = 0;
 // Create gameboard
 const createGameboard = (function () {
   let gameboardContainer = document.querySelector(".gameboard");
+  let gridSquares;
   let grid = ["", "", "", "", "", "", "", "", ""];
   let gridId = 0;
 
@@ -16,37 +17,65 @@ const createGameboard = (function () {
 
   // Add grid event listeners
   const addEventListeners = (function () {
-    let gridSquares = [...document.querySelectorAll(".grid-square")];
+    gridSquares = [...document.querySelectorAll(".grid-square")];
 
     gridSquares.forEach((square) => {
+      square.addEventListener("click", mark);
+    });
+
+    return { gridSquares, mark };
+  })();
+
+  // Check for winners
+  function checkWinners() {
+    if (
+      // Horizontal wins
+      (grid[0] == grid[1] && grid[0] == grid[2] && grid[0] !== "") ||
+      (grid[3] == grid[4] && grid[3] == grid[5] && grid[3] !== "") ||
+      (grid[6] == grid[7] && grid[6] == grid[8] && grid[6] !== "") ||
+      // Vertical wins
+      (grid[0] == grid[3] && grid[0] == grid[6] && grid[0] !== "") ||
+      (grid[1] == grid[4] && grid[1] == grid[7] && grid[4] !== "") ||
+      (grid[2] == grid[5] && grid[2] == grid[8] && grid[2] !== "") ||
+      // Diagonal wins
+      (grid[0] == grid[4] && grid[0] == grid[8] && grid[0] !== "") ||
+      (grid[2] == grid[4] && grid[2] == grid[6] && grid[2] !== "")
+    ) {
+      alert("WINNA!");
+      resetGameboard();
+    }
+
+    // Check if all squares have been filled
+    if (grid.indexOf("") == -1) {
+      alert("TIE");
+    }
+  }
+
+  function mark() {
+    grid[this.dataset.id] = counter % 2 === 0 ? "X" : "O";
+    this.innerText = counter % 2 === 0 ? "X" : "O";
+    this.removeEventListener("click", mark);
+    counter++;
+    setTimeout(checkWinners, 500);
+  }
+
+  function resetGameboard() {
+    for (let i = 0; i < gridSquares.length; i++) {
+      gridSquares[i].innerText = "";
+      grid[i] = "";
+      counter = 1;
+    }
+
+    gridSquares.forEach((square) => {
+      square.removeEventListener("click", mark);
       square.addEventListener("click", function mark() {
         grid[square.dataset.id] = counter % 2 === 0 ? "X" : "O";
         square.innerText = counter % 2 === 0 ? "X" : "O";
         square.removeEventListener("click", mark);
         counter++;
-        checkWinners();
+        setTimeout(checkWinners, 500);
       });
     });
-
-    // Check for winners
-    function checkWinners() {
-      if (
-        // Horizontal wins
-        (grid[0] == grid[1] && grid[0] == grid[2] && grid[0] !== "") ||
-        (grid[3] == grid[4] && grid[3] == grid[5] && grid[3] !== "") ||
-        (grid[6] == grid[7] && grid[6] == grid[8] && grid[6] !== "") ||
-        // Vertical wins
-        (grid[0] == grid[3] && grid[0] == grid[6] && grid[0] !== "") ||
-        (grid[1] == grid[4] && grid[1] == grid[7] && grid[4] !== "") ||
-        (grid[2] == grid[5] && grid[2] == grid[8] && grid[2] !== "") ||
-        // Diagonal wins
-        (grid[0] == grid[4] && grid[0] == grid[8] && grid[0] !== "") ||
-        (grid[2] == grid[4] && grid[2] == grid[6] && grid[2] !== "")
-      ) {
-        alert("WINNA!");
-      }
-    }
-  })();
-
+  }
   return { grid };
 })();
