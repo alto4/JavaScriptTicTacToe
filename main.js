c3deeef95f17395722759a15d9e8748a1dd1610d
Scott Alton <scottaltonmusic@gmail.com>
2021-04-30 00:45:44 -0400

    Added click events and basic check for winning combo.

--- main.js
+++ main.js
@@ -1,16 +1,52 @@
+let counter = 0;
+
 // Create gameboard
 const createGameboard = (function () {
   let gameboardContainer = document.querySelector(".gameboard");
   let grid = ["", "", "", "", "", "", "", "", ""];
   let gridId = 0;
 
-  console.log(gameboardContainer);
   grid.forEach((square) => {
     let gridSquare = `
     <div class="grid-square" data-id="${gridId}">
     </div>`;
-
     gameboardContainer.innerHTML += gridSquare;
     gridId++;
   });
+
+  // Add grid event listeners
+  const addEventListeners = (function () {
+    let gridSquares = [...document.querySelectorAll(".grid-square")];
+
+    gridSquares.forEach((square) => {
+      square.addEventListener("click", function mark() {
+        grid[square.dataset.id] = counter % 2 === 0 ? "X" : "O";
+        square.innerText = counter % 2 === 0 ? "X" : "O";
+        square.removeEventListener("click", mark);
+        counter++;
+        checkWinners();
+      });
+    });
+
+    // Check for winners
+    function checkWinners() {
+      if (
+        // Horizontal wins
+        (grid[0] == grid[1] && grid[0] == grid[2] && grid[0] !== "") ||
+        (grid[3] == grid[4] && grid[3] == grid[5] && grid[3] !== "") ||
+        (grid[6] == grid[7] && grid[6] == grid[8] && grid[6] !== "") ||
+        // Vertical wins
+        (grid[0] == grid[3] && grid[0] == grid[6] && grid[0] !== "") ||
+        (grid[1] == grid[4] && grid[1] == grid[7] && grid[4] !== "") ||
+        (grid[2] == grid[5] && grid[2] == grid[8] && grid[2] !== "") ||
+        // Diagonal wins
+        (grid[0] == grid[4] && grid[0] == grid[8] && grid[0] !== "") ||
+        (grid[2] == grid[4] && grid[2] == grid[6] && grid[2] !== "")
+      ) {
+        alert("WINNA!");
+      }
+    }
+  })();
+
+  return { grid };
 })();
